487e551ec2d2190c07f32eb4fb37ff38ce49ce6d
ssolanki <sushobhitsolanki@gmail.com>
2018-05-27 22:44:39 +0530

    create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'multiindex',
+  packages = ['multiindex'],
+  version = '0.1.0',
+  description = 'Container similar to boost multi index',
+  author = 'Sushobhit',
+  author_email = 'sushobhitsolanki@gmail.com',
+  url = 'https://github.com/sushobhit27/multiindex',
+  download_url = 'https://github.com/sushobhit27/multiindex/archive/0.1.tar.gz',
+  keywords = ['container', 'multi index', 'boost'], # arbitrary keywords
+  classifiers = [],
+)
