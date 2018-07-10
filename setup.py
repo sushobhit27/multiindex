9d2d73770fffca5b6a786f9d4171a6e23d295922
ssolanki <sushobhitsolanki@gmail.com>
2018-07-10 12:12:22 +0530

    add classifiers in setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,18 @@ setup(
   url = 'https://github.com/sushobhit27/multiindex',
   download_url = 'https://github.com/sushobhit27/multiindex/archive/0.1.tar.gz',
   keywords = ['container', 'multi index', 'boost'], # arbitrary keywords
-  classifiers = [],
+  classifiers = [
+      'Development Status :: 4 - Beta',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: Implementation :: CPython',
+      'Operating System :: Microsoft',
+      'Operating System :: Microsoft :: Windows :: Windows 7',
+      'Operating System :: Unix',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Utilities',
+      'Intended Audience :: Developers',
+      ],
 )
